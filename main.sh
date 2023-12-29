8220af13d1f0b82a836439d96e35353214d8783c
lutos5 <86042308+vonaxs@users.noreply.github.com>
2023-12-29 20:27:05 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -40,14 +40,15 @@ isIPChanged() {
     newIP=$(curl ip.sb)
     if [[ -n $oidIP ]]; then
 	if [[ $oidIP != $newIP ]]; then
-            sudo sh -c "$newIP > $ip_file"
+            sudo sh -c "echo $newIP > $ip_file"
             sudo sh -c "echo \$(date)：IP发生了改变，检测新的IP是否可以解锁媒体... >> $log_file"
             checkIP
 	fi
     else
-	sudo sh -c "echo \$(date)：$newIP > $ip_file"
+	sudo sh -c "echo $newIP > $ip_file"
     fi
 }
+
 if [[ "$1" == "isIPChanged" ]]; then
     echo "正在检测IP是否发生了改变..."
     isIPChanged
