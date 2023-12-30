04e8240a4eb17eabbb48e27b93e1c378258bf7fa
lutos5 <86042308+vonaxs@users.noreply.github.com>
2023-12-30 10:08:20 +0800

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+log_file="/root/mediaCheck/change.log"
+isIPChanged_file="/root/mediaCheck/isIPChanged.txt"
+threshold=100	# 如果行数超过阈值，覆盖文件
+
+# 更换IP
+changeIP() {
+    sudo sh -c "echo \$(date)：正在更换IP... >> $log_file"
+    api=$(cat /root/mediaCheck/.api)
+    for ((i = 0; i < 5; i++)); do
+        sudo sh -c "echo 1" > $isIPChanged_file
+        result=$(curl -s "$api")
+        if ! echo "$result" | grep -q '"ok":true'; then
+            sudo sh -c "echo -n > $isIPChanged_file"  # 清空文件
+            sudo sh -c "echo \$(date): 更换失败，等待 120 秒后重试... $result" >> $log_file
+            sleep 120
+        fi
+    done
+}
+
+# 检测IP是否可以解锁媒体
+checkIP() {
+    sudo sh -c "echo \$(date)：正在检测IP是否可以解锁媒体... >> $log_file"
+    # 访问此网址，如果无法观看非自制剧，会返回"Netflix"
+    title=$(curl -s https://www.netflix.com/tw/title/70143836 | grep -oP '<title>\K[^<]*')
+    if [[ $title == 'Netflix' ]]; then
+	sudo sh -c "echo \$(date)：当前IP无法解锁Netflix，准备更换IP... >> $log_file"
+        echo "当前IP无法解锁Netflix，准备更换IP..."
+        changeIP
+    else
+	sudo sh -c "echo \$(date)：当前IP可以解锁Netflix，无需更换IP... >> $log_file"
+        echo "当前IP可以解锁Netflix，无需更换IP..."
+    fi
+}
+
+# 主函数
+echo "正在检测IP是否可以解锁媒体..."
+checkIP
