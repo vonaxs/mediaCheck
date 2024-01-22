ea5fc0059a47ad184e8a7c62a599b935eb5b8f81
lutos5 <86042308+vonaxs@users.noreply.github.com>
2024-01-22 17:56:50 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -52,6 +52,11 @@ checkIP() {
 }
 
 # 主函数
-echo "正在检测IP是否可以解锁媒体..."
-checkIP
+random_number=$((RANDOM % 10))
+# 随机检测，避免Netflix识别在定时检测
+if [ "$random_number" -eq 0 ]; then
+    echo "正在检测IP是否可以解锁媒体..."
+    sleep $random_number
+    checkIP
+fi
 
