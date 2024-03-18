561fd1886f65f2661cd3c928681b98dfb4548ed4
lutos5 <86042308+vonaxs@users.noreply.github.com>
2024-03-18 13:07:00 +0800

    Update change.sh

--- change.sh
+++ change.sh
@@ -7,7 +7,7 @@ threshold=500	# 如果行数超过阈值，覆盖文件
 
 # 更换IP
 changeIP() {
-    ip=$(curl ip.sb)
+    ip=$(curl -s ip.sb)
     echo "当前的IP为 $ip"
     sudo sh -c "echo \$(date)：当前的IP为：$ip，正在更换IP... >> $log_file"
     api=$(cat /root/mediaCheck/api.txt)
@@ -15,7 +15,7 @@ changeIP() {
         sudo sh -c "echo 1" > $isIPChanged_file
         result=$(curl -s "$api")
         if echo "$result" | grep -q '"ok":true'; then
-            ip=$(curl ip.sb)
+            ip=$(curl -s ip.sb)
             sudo sh -c "echo \$(date): 更换IP成功，新的IP为：$ip $result" >> $log_file
             break
         else
