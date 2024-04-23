653b25e7ebde9421d4d67b582141b509e79fd4c9
lutos5 <86042308+vonaxs@users.noreply.github.com>
2024-04-23 19:20:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,10 +54,10 @@ chmod +x /root/mediaCheck/check.sh
 # 如果定时任务为空，直接创建，否则追加
 # 如果任务不存在，则创建
 if [ -z "$(crontab -l)" ]; then
-    (echo "0 */8 * * * /root/mediaCheck/change.sh") | crontab -
+    (echo "0 */6 * * * /root/mediaCheck/change.sh") | crontab -
 else
     if ! crontab -l | grep -q "/root/mediaCheck/change.sh"; then
-        (crontab -l ; echo "0 */8 * * * /root/mediaCheck/change.sh") | crontab -
+        (crontab -l ; echo "0 */6 * * * /root/mediaCheck/change.sh") | crontab -
     fi
 fi
 # if ! crontab -l | grep -q "/root/mediaCheck/isIPChanged.sh"; then
